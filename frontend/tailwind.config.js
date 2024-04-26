9e03cafb7573da43de7503aae23635cfa8122260
DukeSchlegel <dukesc9@gmail.com>
2024-04-26 11:00:41 +0200

    Added screen sizes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,15 @@ export default {
     "./src/**/**/**/**/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+      '4xl': '2480px',
+    },
     extend: {
       fontFamily: {
         'roboto': ['Roboto', 'sans-serif'],
