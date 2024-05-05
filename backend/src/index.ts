0782cf76670a608746423112e5eb271aeee5520b
maxip <maxwell.pechmann@gmail.com>
2024-05-05 19:21:46 +0200

    created index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,13 @@
-import Koa from "koa"
-import Router from "koa-router";
+import {createApp} from "./app";
+import {BackendConfig} from "./config";
 
-const app = new Koa();
-const router = new Router();
+export function runApp() {
+    const app = createApp()
+    const config = new BackendConfig()
 
-router.get("/", (ctx, next) => {
-    ctx.body = "Hello World!";
-})
+    app.listen(config.loadPort(), config.loadHost(),() => {
+        console.log("Server is running.")
+    })
+}
 
-router.get("/test", (ctx, next) => {
-    ctx.body = "this is a test.";
-})
-
-app.use(router.routes())
-app.use(router.allowedMethods())
-
-app.listen(3333, () => {
-    console.log("Server started on localhost:3333");
-})
+runApp()
