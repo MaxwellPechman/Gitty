ae537ccbd1c2b43de1aaf3f2ab7b099a20de4314
DukeSchlegel <dukesc9@gmail.com>
2024-05-14 15:53:26 +0200

    small Changes on LandingPage.tsx

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -17,7 +17,7 @@ export default {
       'lg': '1024px',
       'xl': '1280px',
       '2xl': '1536px',
-      '3xl': '1920px',
+      '3xl': '1921px',
       '4xl': '2480px',
     },
     extend: {
