924bb70fa1b8e4999a75e457a2ad47eefa95c9ee
DukeSchlegel <dukesc9@gmail.com>
2024-05-29 10:22:30 +0200

    made Tasks loaded from the Database

--- frontend/src/components/projects/Task.tsx
+++ frontend/src/components/projects/Task.tsx
@@ -1,40 +1,35 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "./ag-theme-TaskGrid.css";
+import {getUserTasks, requestUserId} from "../../api/Api.ts";
 
+export interface ITask {
+    Taskname: string,
+    Project: string,
+    Status: string,
+    Action: string
+}
 
 export function Tasks() {
-    var task = [
-        {Taskname: "Tesla", Project: "Songwebsite", Working: 1, Status: getStatus(1), Action: "..."},
-        {Taskname: "Ford", Project: "Code for a friend", Working: 2, Status: getStatus(2), Action: "..."},
-        {Taskname: "Toyota", Project: "Mobile game for IPhone", Working: 3, Status: getStatus(3), Action: "..."},
-        {Taskname: "Tesla", Project: "Songwebsite", Working: 1, Status: getStatus(1), Action: "..."},
-    ]
+    const [tasks, setTasks] = useState<ITask[]>([])
+    const [userId, setUserId] = useState<requestUserId>({uid: 2})
 
-    const [rowData, setRowData] = useState(task)
-    const [colDefs, setColDefs] = useState([
+    useEffect(() => {
+        getUserTasks(userId).then((data) => {
+            setTasks(data)
+        })
+        return
+    }, [])
+
+    const [colDefs] = useState([
         {field: "Taskname", flex: 10},
         {field: "Project", flex: 3},
-        {field: "Working", flex: 2},
         {field: "Status", flex: 2},
         {field: "Action", flex: 2}
     ])
 
     return <div className="ag-theme-TaskGrid" style={{height: 350}}>
-        <AgGridReact rowData={rowData} columnDefs={colDefs}/>
+        <AgGridReact rowData={tasks} columnDefs={colDefs}/>
     </div>
-}
-
-function getStatus(projectStatus: number) {
-    switch (projectStatus) {
-        case 1:
-            return "active"
-        case 2:
-            return "done"
-        case 3:
-            return "canceled"
-        default:
-            return "new"
-    }
 }
