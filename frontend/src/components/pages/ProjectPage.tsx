db00db24898507516efc370fc450b049da3fd3bd
DukeSchlegel <dukesc9@gmail.com>
2024-05-31 09:29:20 +0200

    Added ProjectDetails to ProjectPage.tsx

--- frontend/src/components/pages/ProjectPage.tsx
+++ frontend/src/components/pages/ProjectPage.tsx
@@ -1,8 +1,15 @@
 import {Topnav} from "../topnav/Topnav.tsx";
 import {Projects} from "../projects/Project.tsx";
 import {Tasks} from "../projects/Task.tsx";
+import {ProjectDetails} from "../projects/ProjectDetails.tsx";
+import {useState} from "react";
 
 export function ProjectPage() {
+    const [showPrjoectsTab, setShowPrjoectsTab] = useState(false);
+
+    function toggleView() {
+        setShowPrjoectsTab((prev) => !prev)
+    }
 
     return (
         <div className="w-screen h-screen bg-code-grey-950">
@@ -11,15 +18,18 @@ export function ProjectPage() {
             <div className="w-9/12 h-[363px]">
                     <span className="text-[30px] text-code-grey-500 ml-4">My projects</span>
                     <button
-                        className="ml-[31px] border-code-border-gray border-[1px] text-[14px] p-1 rounded-xl text-white w-40 hover:bg-white hover:text-black">+
+                        className="ml-[31px] border-code-border-gray border-[1px] text-[14px] p-1 rounded-xl text-white w-40 hover:bg-white hover:text-black"
+                        onClick={toggleView}>+
                         Create project
                     </button>
                     <div
                         className="bg-code-grey-800 h-[308px] rounded-2xl mt-4 border-code-border-projects border-[1px] flex overflow-x-scroll noScrollbar">
-                        <span className="hidden">Projekte Laden</span>
                         <Projects />
                     </div>
                 </div>
+            </div>
+            <div className={showPrjoectsTab ? "float-right" : "hidden"}>
+                <ProjectDetails/>
             </div>
             <div className="flex justify-around mt-[37px]">
                 <div className="w-9/12 h-[363px]">
@@ -31,7 +41,6 @@ export function ProjectPage() {
                     <div className="mt-4">
                         <Tasks/>
                     </div>
-
                 </div>
             </div>
         </div>
