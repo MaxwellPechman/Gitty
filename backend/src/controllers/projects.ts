3d0352546b791d77c121eedc90c01d294468beb4
DukeSchlegel <dukesc9@gmail.com>
2024-05-31 10:39:11 +0200

    changed create Project function

--- backend/src/controllers/projects.ts
+++ backend/src/controllers/projects.ts
@@ -1,9 +1,11 @@
 import {PostgresClient} from "../db/db";
 import {SQLFileManager} from "../db/sql";
-import {Project} from "../types/project";
+import {newProject} from "../types/project";
 
-export async function createProject(db: PostgresClient, sql: SQLFileManager, projectData: Project) {
-    const data = [projectData.projectName, projectData.projectType]
+export async function createProject(db: PostgresClient, sql: SQLFileManager, projectData: newProject) {
+    const dataNewProject = [projectData.projectName, projectData.projectType]
+    db.execute(sql.getSQLStatement("createProject.sql"), dataNewProject).then(() => {})
+    const dataLinkUser = [projectData.uid]
+    await db.execute(sql.getSQLStatement("createLinkUserProject.sql"), dataLinkUser)
 
-    await db.execute(sql.getSQLStatement("createProject.sql"), data)
 }
