1fbf022c88b3b51576df0189d0c14a81c1cde80b
DukeSchlegel <dukesc9@gmail.com>
2024-05-31 09:29:47 +0200

    Added new backend route for projects

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -2,7 +2,10 @@ import Router from "koa-router";
 import {PostgresClient} from "./db/db";
 import {SQLFileManager} from "./db/sql";
 import {registerUser, getUserProjects, getUserTasks} from "./controllers/user";
-import {UserRegister, requestUserId} from "./types/user";
+import {UserRegister, requestId} from "./types/user";
+import {getTypes} from "./controllers/types";
+import {Project} from "./types/project";
+import {createProject} from "./controllers/projects";
 
 export function createRouter(db: PostgresClient, sql: SQLFileManager) {
     const router = new Router();
@@ -23,12 +26,26 @@ export function createRouter(db: PostgresClient, sql: SQLFileManager) {
     })
 
     router.post("/api/getUserProjects", async (ctx) => {
-       ctx.body = await getUserProjects(db, ctx.request.body as requestUserId)
+       ctx.body = await getUserProjects(db, ctx.request.body as requestId)
     })
 
     router.post("/api/getUserTasks", async (ctx) => {
-        ctx.body = await getUserTasks(db, ctx.request.body as requestUserId)
+        ctx.body = await getUserTasks(db, ctx.request.body as requestId)
     })
 
+    router.post("/api/getTypes", async (ctx) => {
+        ctx.body = await getTypes(db, ctx.request.body as requestId)
+    })
+
+    router.post("/api/createProject", (ctx) => {
+        createProject(db, sql, ctx.request.body as Project)
+            .then(() => {
+                ctx.body = "create"
+            })
+            .catch((err) => {
+                ctx.body = "error"
+                console.log(err);
+            })
+    })
     return router;
 }
