3f070c6d7f72f16496dbba2d1d26c2656f2edc14
DukeSchlegel <dukesc9@gmail.com>
2024-06-01 22:09:57 +0200

    changed sql to sql file

--- backend/src/controllers/types.ts
+++ backend/src/controllers/types.ts
@@ -2,12 +2,8 @@ import {PostgresClient} from "../db/db";
 import {SQLFileManager} from "../db/sql";
 import {requestId} from "../types/user";
 
-export async function getTypes(db: PostgresClient, type_classification: requestId) {
+export async function getTypes(db: PostgresClient, sql: SQLFileManager, type_classification: requestId) {
     const values = [type_classification.id]
-    const SQL = `
-    SELECT typeId, type_name FROM types
-    WHERE type_classification = $1;
-    `
 
-    return await db.query(SQL, values)
+    return await db.query(sql.getSQLStatement("selectTypes.sql"), values)
 }
