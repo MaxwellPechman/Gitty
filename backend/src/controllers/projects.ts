5768a6b636598923c939bda60764d53255c82d9b
DukeSchlegel <dukesc9@gmail.com>
2024-06-01 22:09:22 +0200

    added project creation

--- backend/src/controllers/projects.ts
+++ backend/src/controllers/projects.ts
@@ -3,9 +3,19 @@ import {SQLFileManager} from "../db/sql";
 import {newProject} from "../types/project";
 
 export async function createProject(db: PostgresClient, sql: SQLFileManager, projectData: newProject) {
-    const dataNewProject = [projectData.projectName, projectData.projectType]
-    db.execute(sql.getSQLStatement("createProject.sql"), dataNewProject).then(() => {})
-    const dataLinkUser = [projectData.uid]
-    await db.execute(sql.getSQLStatement("createLinkUserProject.sql"), dataLinkUser)
+    var typeId = await db.query(sql.getSQLStatement("selectSingleType.sql"), [projectData.projectType])
 
+    const dataNewProject = [projectData.projectName, typeId[0].typeid]
+
+    var pid = await db.query(sql.getSQLStatement("createProject.sql"), dataNewProject);
+
+    const dataLink = [pid[0].pid, projectData.uid]
+
+    await createUserLink(db, sql, dataLink)
+
+    return true
+}
+
+export async function createUserLink(db: PostgresClient, sql: SQLFileManager, data: any[]) {
+    await db.execute(sql.getSQLStatement("createLinkUserProject.sql"), data)
 }
