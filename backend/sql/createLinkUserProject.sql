5b3049ddf1109d45c6981eb5e0e0b4dc57e9630b
DukeSchlegel <dukesc9@gmail.com>
2024-06-01 22:08:24 +0200

    added new SQL files

--- backend/sql/createLinkUserProject.sql
+++ backend/sql/createLinkUserProject.sql
@@ -1,2 +1,2 @@
 INSERT INTO projectsusers (pid, uid, owner)
-SELECT MAX(PID), $1, TRUE from projects
+VALUES ($1, $2, TRUE);
--- backend/sql/createProject.sql
+++ backend/sql/createProject.sql
@@ -1,2 +1,2 @@
 INSERT INTO projects (project_name, project_type, date_created, date_changed, active)
-VALUES ($1, $2, CURRENT_DATE, CURRENT_DATE, TRUE);
+VALUES ($1, $2, CURRENT_DATE, CURRENT_DATE, TRUE) RETURNING pid;
