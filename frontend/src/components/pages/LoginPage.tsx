2726dc2987b7caa7f1aa76422cd7b356a7eee7c0
maxip <maxwell.pechmann@gmail.com>
2024-06-02 20:04:14 +0200

    LoginPage.tsx in now stores session key

--- frontend/src/components/pages/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage.tsx
@@ -2,13 +2,23 @@ import logo from "../../assets/icons/Gitty_Logo@2.png"
 import lock_icon from "../../assets/icons/lock2.png";
 import {useNavigate} from "react-router-dom";
 import {useState} from "react";
-import {requestUserLogin, UserLogin} from "../../api/Api.ts";
+import {requestUserLogin} from "../../api/Api.ts";
+import {UserLogin} from "../../types/user.ts";
 
 export function LoginPage() {
-
     const [loginData, setLoginData] = useState({} as UserLogin)
-
     const navigate = useNavigate();
+
+    function loginUser() {
+        requestUserLogin(loginData)
+            .then((response) => {
+                if(response.session !== undefined) {
+                    localStorage.setItem("session-key", response.session)
+                    navigate("/projects")
+                }
+        })
+    }
+
     return (
         <div className="flex flex-col w-screen h-screen justify-center bg-code-grey-950">
             <div className="flex flex-col items-center justify-center">
@@ -19,16 +29,16 @@ export function LoginPage() {
                 <form className="p-6 w-[459px] bg-code-grey-800 rounded-xl flex flex-col gap-y-5">
                     <div className="flex flex-col gap-y-3">
                         <div className="flex gap-x-2">
-                            <label className="font-roboto text-white" htmlFor="mail">Email</label>
+                            <label className="font-roboto text-white" htmlFor="username">Username</label>
                             <div className="font-roboto text-code-red">*</div>
                         </div>
                         <input
                             className="px-4 py-5 bg-code-grey-950 font-roboto rounded-xl border border-code-grey-600 text-code-grey-500 focus:outline-none focus:text-white focus:border-code-grey-300 hover:border-code-grey-300 transition duration-200 ease-in-out"
                             type="text"
-                            name="mail"
-                            placeholder="Enter your email"
+                            name="username"
+                            placeholder="Enter your username"
                             onChange={(event) => setLoginData({
-                                mail: event.target.value,
+                                username: event.target.value,
                                 password: loginData.password,
                                 remember: loginData.remember
                         })}/>
@@ -47,7 +57,7 @@ export function LoginPage() {
                                 name="password"
                                 placeholder="Enter your password"
                                 onChange={(event) => setLoginData({
-                                    mail: loginData.mail,
+                                    username: loginData.username,
                                     password: event.target.value,
                                     remember: loginData.remember
                                 })}/>
@@ -56,17 +66,14 @@ export function LoginPage() {
                     <div className="flex gap-y-3 ml-1">
                         <input className="w-5 accent-transparent" type="checkbox"
                                 onChange={(event) => setLoginData({
-                                    mail: loginData.mail,
+                                    username: loginData.username,
                                     password: loginData.password,
-                                    remember: event.target.value
+                                    remember: false
                                 })}/>
                         <label className="ml-3 text-lg">Remember me</label>
                     </div>
                     <button className="my-2 py-2 bg-white text-black font-roboto font-bold rounded-xl" type="button"
-                            onClick={() => requestUserLogin(loginData).then((response) => {
-                                navigate("/projects")
-                                console.log(response)
-                            })}>Login</button>
+                            onClick={() => loginUser()}>Login</button>
                     <div className="-mt-3 flex gap-x-2 justify-center">
                         <div className="text-code-grey-500">No account?</div>
                         <button className="text-code-blue hover:underline" onClick={() => navigate("/register")}>Register now
