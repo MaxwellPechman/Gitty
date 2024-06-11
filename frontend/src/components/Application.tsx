d4a5d804a4a9b83b8a62dfdb0875bc5f27f573ba
DukeSchlegel <dukesc9@gmail.com>
2024-06-11 09:33:37 +0200

    neue Navigationsstruktur eingefügt

--- frontend/src/components/Application.tsx
+++ frontend/src/components/Application.tsx
@@ -1,16 +1,20 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {LandingPage} from "./pages/LandingPage.tsx";
-import {ProjectPage} from "./pages/ProjectPage.tsx";
+import {HomePage} from "./pages/HomePage.tsx";
 import {RegisterPage} from "./pages/RegisterPage.tsx";
 import {LoginPage} from "./pages/LoginPage.tsx";
 import {ProfilePage} from "./pages/ProfilePage.tsx";
+import {ProjectsPage} from "./pages/ProjectsPage.tsx";
+import {TasksPage} from "./pages/TasksPage.tsx";
 
 export function Application() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage/>}/>
-                <Route path="/projects" element={<ProjectPage/>}/>
+                <Route path="/home" element={<HomePage/>}/>
+                <Route path="/projects" element={<ProjectsPage/>}/>
+                <Route path="/tasks" element={<TasksPage/>}/>
                 <Route path="/register" element={<RegisterPage/>}/>
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path="/profile" element={<ProfilePage/>}/>
