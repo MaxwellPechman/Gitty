ed379e9dec08165fb2707550f2c757b41ccd1b9b
DukeSchlegel <dukesc9@gmail.com>
2024-06-24 21:45:54 +0200

    small changes

--- frontend/src/api/Api.ts
+++ frontend/src/api/Api.ts
@@ -71,7 +71,7 @@ export async function fetchFileSystem(pid: requestId): Promise<any> {
     return post(url, pid)
 }
 
-export async function createFolder(pid: number, parent_dir: number, dirName: string): Promise<any> {
+export async function createFolder(pid: number, parent_dir: number | null, dirName: string): Promise<any> {
     const url = getBackendBaseUrl() + "/createFolder"
     const data = [pid, parent_dir, dirName]
     return post(url, data)
