b4e3d759167d807f389dbcba1c16c921ba75bf0a
maxip <maxwell.pechmann@gmail.com>
2024-06-25 14:33:14 +0200

    fixed unused useState

--- frontend/src/components/pages/TasksPage.tsx
+++ frontend/src/components/pages/TasksPage.tsx
@@ -1,8 +1,6 @@
-import {useState} from "react";
 import {Topnav} from "../topnav/Topnav.tsx";
 
 export function TasksPage() {
-    const [userId, setUserId] = useState(1);
 
     return (
         <div className="w-screen h-screen bg-code-grey-950">
