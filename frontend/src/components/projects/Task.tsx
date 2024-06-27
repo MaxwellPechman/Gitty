aacbd1ff49cf3b4cd288d91255a23ba2465ae7a9
DukeSchlegel <dukesc9@gmail.com>
2024-06-27 18:21:31 +0200

    Tasks angepasst

--- frontend/src/components/projects/Task.tsx
+++ frontend/src/components/projects/Task.tsx
@@ -2,9 +2,10 @@ import {useEffect, useState} from "react";
 import { AgGridReact } from 'ag-grid-react';
 import "ag-grid-community/styles/ag-grid.css";
 import "./ag-theme-TaskGrid.css";
-import {getUserTasks} from "../../api/Api.ts";
+import {getUserTasks, updateTaskStatus} from "../../api/Api.ts";
 
 export interface ITask {
+    tid: number,
     Taskname: string,
     Project: string,
     Status: string,
@@ -18,17 +19,33 @@ export function Tasks() {
         getUserTasks(userId).then((data) => {
             setTasks(data)
         })
-        return
     }, [])
 
+    const actionComponent = (props: any) => {
+        return (
+            <select className="w-full h-full bg-code-grey-800"
+                    defaultValue={props.data.Status}
+                    onChange={(event) => {
+                        updateTaskStatus(Number(event.target.value), props.data.tid)
+                    }}>
+                <option value="0">new</option>
+                <option value="1">active</option>
+                <option value="2">done</option>
+                <option value="3">canceled</option>
+            </select>
+        )
+    }
+
     const [colDefs] = useState<any>([
-        {field: "Task name", flex: 10},
+        {field: "Taskname", flex: 10},
         {field: "Project", flex: 3},
-        {field: "Status", flex: 2},
+        {field: "Status", flex: 2, cellRenderer: actionComponent},
         {field: "Action", flex: 2}
     ])
 
-    return <div className="ag-theme-TaskGrid" style={{height: 350}}>
-        <AgGridReact rowData={tasks} columnDefs={colDefs}/>
-    </div>
+    return (
+        <div className="ag-theme-TaskGrid" style={{height: 350}}>
+            <AgGridReact rowData={tasks} columnDefs={colDefs} className=""/>
+        </div>
+    )
 }
