e415cce5e20309681562bdebbeac4ce78309d424
MaxPechmann <pechmann@ambright.de>
2024-06-30 17:15:51 +0200

    created task.ts + updated task controller

--- backend/src/controllers/task.ts
+++ backend/src/controllers/task.ts
@@ -3,13 +3,10 @@ import {SQLFileManager} from "../db/sql";
 import {newTask} from "../types/project";
 
 export async function createTask(db: PostgresClient, sql: SQLFileManager, taskData: newTask) {
-    var uid = await db.query(sql.getSQLStatement("selectUserIdBySessionId.sql"), [taskData.uid])
-
-    var typeId = await db.query(sql.getSQLStatement("selectSingleProject.sql"), [taskData.taskPid, uid[0].uid])
-
+    const uid = await db.query(sql.getSQLStatement("selectUserIdBySessionId.sql"), [taskData.uid]);
+    const typeId = await db.query(sql.getSQLStatement("selectSingleProject.sql"), [taskData.taskPid, uid[0].uid]);
     const dataNewTask = [taskData.taskName, taskData.taskStatus, taskData.taskDescription, typeId[0].pid]
-
-    var tid = await db.query(sql.getSQLStatement("createTask.sql"), dataNewTask);
+    const tid = await db.query(sql.getSQLStatement("createTask.sql"), dataNewTask);
     const dataLink = [tid[0].tid, uid[0].uid]
 
     await createUserLink(db, sql, dataLink)
---
+++ backend/src/types/task.ts
@@ -0,0 +1,7 @@
+export type Task = {
+    tid: number
+    task_name: string
+    project_name: string
+    status: number
+    action: string
+}
