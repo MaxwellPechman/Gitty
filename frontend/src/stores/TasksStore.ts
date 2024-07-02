dc034502c3b4654f717ac17901c5b2495d2cc6b2
maxip <maxwell.pechmann@gmail.com>
2024-07-02 13:42:37 +0200

    created TasksStore.ts

--- frontend/src/stores/TasksStore.ts
+++ frontend/src/stores/TasksStore.ts
@@ -1,2 +1,19 @@
+import {Task} from "../types/task.ts";
+import {create} from "zustand";
+
 interface TasksStore {
+    tasks: Task[];
+    setTasks: (tasks: Task[]) => void;
+    updateTasks: (tasks: Task[]) => void;
 }
+
+export const useTasksStore = create<TasksStore>((set) => ({
+    tasks: [],
+
+    setTasks: (tasks) => set({ tasks }),
+
+    updateTasks: (tasks) =>
+        set((state) => ({
+            tasks: [...state.tasks, ...tasks],
+    }))
+}))
