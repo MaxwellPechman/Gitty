12895cd8ef0c8f713046a25bce13417368ee6087
DukeSchlegel <dukesc9@gmail.com>
2024-07-02 15:30:48 +0200

    Test for Deployment

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -37,7 +37,7 @@ export class BackendConfig {
     }
 
     public loadPort(): number {
-        const port = process.env.PORT
+        const port = process.env.SERVER_PORT
 
         if(port === undefined) {
             throw Error("Undefined server port.")
