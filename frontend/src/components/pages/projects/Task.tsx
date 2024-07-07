96e36513d5a68fce424c980d48d621015ef4adaa
DukeSchlegel <dukesc9@gmail.com>
2024-07-07 13:45:30 +0200

    Layout von Tasks angepasst

--- frontend/src/components/pages/projects/Task.tsx
+++ frontend/src/components/pages/projects/Task.tsx
@@ -1,18 +1,20 @@
 import {useEffect, useState} from "react";
-import { AgGridReact } from 'ag-grid-react';
 import "ag-grid-community/styles/ag-grid.css";
 import "./ag-theme-TaskGrid.css";
-import {requestUserTasks, updateTaskStatus} from "../../../api/Api.ts";
+import {requestUserTasks} from "../../../api/Api.ts";
+import {useNavigate} from "react-router-dom";
 
 export interface ITask {
     tid: number,
     Taskname: string,
-    Project: string,
-    Status: string,
-    Action: string
+    Project: undefined | string,
+    Status: number,
+    Action: undefined | string,
+    Description: undefined | string
 }
 
 export function Tasks() {
+    const navigate = useNavigate();
     const [tasks, setTasks] = useState<ITask[]>([])
     const [userId] = useState<string>(localStorage.getItem("sessionID") || "")
     useEffect(() => {
@@ -21,31 +23,47 @@ export function Tasks() {
         })
     }, [])
 
-    const actionComponent = (props: any) => {
-        return (
-            <select className="w-full h-full bg-code-grey-800"
-                    defaultValue={props.data.Status}
-                    onChange={(event) => {
-                        updateTaskStatus(Number(event.target.value), props.data.tid)
-                    }}>
-                <option value="0">new</option>
-                <option value="1">active</option>
-                <option value="2">done</option>
-                <option value="3">canceled</option>
-            </select>
-        )
-    }
-
-    const [colDefs] = useState<any>([
-        {field: "Taskname", flex: 10},
-        {field: "Project", flex: 3},
-        {field: "Status", flex: 2, cellRenderer: actionComponent},
-        {field: "Action", flex: 2}
-    ])
-
     return (
-        <div className="ag-theme-TaskGrid" style={{height: 350}}>
-            <AgGridReact rowData={tasks} columnDefs={colDefs} className=""/>
+        <div className="w-full border-code-login-gray border-[1px] rounded-2xl p-2">
+            <div className="flex flex-row my-2">
+                <span className="w-[50%]">Task name</span>
+                <span className="w-[25%]">Project</span>
+                <span className="w-[15%]">Status</span>
+                <span className="w-[15%]">Action</span>
+            </div>
+            <hr/>
+            {tasks.map((task: ITask) => {
+                let status: string
+                switch (task.Status) {
+                    case 0:
+                        status = "new"
+                        break
+                    case 1:
+                        status = "active"
+                        break
+                    case 2:
+                        status = "done"
+                        break
+                    default:
+                        status = "canceled"
+                        break
+                }
+
+                return (
+                    <div key={task.tid}>
+                        <div className="flex flex-row py-3 hover:bg-code-grey-500"
+                             onClick={() => {
+                                 navigate("/task/" + task.tid)
+                             }}>
+                            <span className="w-[50%]">{task.Taskname}</span>
+                            <span className="w-[25%]">{task.Project}</span>
+                            <span className="w-[15%]">{status}</span>
+                            <button className="w-[15%] text-start h-full">...</button>
+                        </div>
+                        <hr />
+                    </div>
+                )
+            })}
         </div>
     )
 }
