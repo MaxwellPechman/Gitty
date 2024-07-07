9fabd899519bacc3690778a4cf08c258a907567f
maxip <maxwell.pechmann@gmail.com>
2024-07-07 18:22:28 +0200

    debugging createTask

--- backend/src/controllers/task.ts
+++ backend/src/controllers/task.ts
@@ -6,10 +6,15 @@ export async function createTask(db: PostgresClient, sql: SQLFileManager, taskDa
     console.log(taskData);
 
     const uid = await db.query(sql.getSQLStatement("selectUserIdBySessionId.sql"), [taskData.uid]);
+    console.log("1 geht")
     const typeId = await db.query(sql.getSQLStatement("selectSingleProject.sql"), [taskData.taskPid, uid[0].uid]);
+    console.log("2 geht")
     const dataNewTask = [taskData.taskName, taskData.taskStatus, taskData.taskDescription, typeId[0].pid]
+    console.log("3 geht")
     const tid = await db.query(sql.getSQLStatement("createTask.sql"), dataNewTask);
+    console.log("4 geht")
     const dataLink = [tid[0].tid, uid[0].uid]
+    console.log("5 geht")
 
     await createUserLink(db, sql, dataLink)
 
