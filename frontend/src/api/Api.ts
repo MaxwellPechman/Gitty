c29d6bbcaaa0124d5ab5c9adc0b51d3a6d3bf941
DukeSchlegel <dukesc9@gmail.com>
2024-07-08 18:03:55 +0200

    Route für Statusupdate bei Projekten eingefügt

--- frontend/src/api/Api.ts
+++ frontend/src/api/Api.ts
@@ -103,6 +103,13 @@ export async function updateTaskStatus(status: number, tid: number): Promise<any
     return post(url, data)
 }
 
+export async function updateProjectStatus(status: boolean, pid: number): Promise<any> {
+    const url = getBackendBaseUrl() + "/updateProjectStatus"
+    const data = [status, pid]
+
+    return post(url, data)
+}
+
 export async function uploadFile(pid: number, parent_dir: number | null, fileName: string, fileContent: unknown): Promise<any> {
     const url = getBackendBaseUrl() + "/uploadFile"
     const data = [pid, parent_dir, fileName, fileContent]
