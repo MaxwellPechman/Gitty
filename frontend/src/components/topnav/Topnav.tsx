7314b1eb01df6c9140edd771c2386ca7281008a2
maxip <maxwell.pechmann@gmail.com>
2024-07-08 15:40:42 +0200

    updated Topnav

--- frontend/src/components/topnav/Topnav.tsx
+++ frontend/src/components/topnav/Topnav.tsx
@@ -16,7 +16,13 @@ export function Topnav() {
                 <img className="mx-6 mt-2 w-[30px] h-[29px]" src={gitty_icon} alt="gitty_icon"/>
                 <ul className="flex gap-x-4">
                     <li className="text-white leading-8 cursor-pointer hover:text-neutral-500 transition duration-200 ease-in-out"
-                        onClick={() => navigate("/projects")}>Home
+                        onClick={() => navigate("/")}>Home
+                    </li>
+                    <li className="text-white leading-8 cursor-pointer hover:text-neutral-500 transition duration-200 ease-in-out"
+                        onClick={() => navigate("/projects")}>Projects
+                    </li>
+                    <li className="text-white leading-8 cursor-pointer hover:text-neutral-500 transition duration-200 ease-in-out"
+                        onClick={() => navigate("/profile")}>Profile
                     </li>
                 </ul>
             </div>
