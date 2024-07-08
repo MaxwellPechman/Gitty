c90eca24ae6b63277fa28f0d049e3a3911146570
DukeSchlegel <dukesc9@gmail.com>
2024-07-08 12:11:09 +0200

    Refactor SQL

--- backend/sql/selectProjectTasks.sql
+++ backend/sql/selectProjectTasks.sql
@@ -1,12 +1,8 @@
 SELECT
+    tasks.tid AS "tid",
     tasks.task_name AS "Taskname",
     projects.project_name AS "Project",
-    CASE task_status
-        WHEN 0 THEN 'new'
-        WHEN 1 THEN 'in progress'
-        WHEN 2 THEN 'cancled'
-        WHEN 3 THEN 'done'
-    END as "Status",
+    tasks.task_status as "Status",
     '...' AS "Action"
 FROM tasks
 JOIN projects ON tasks.pid = projects.pid
